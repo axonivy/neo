567529f4f2e06b7c6bbc623046f2b373e671f3c9
Lukas Lieb <lukas.lieb@axonivy.com>
2024-06-06 12:47:19 +0200

    Add process function

--- app/data/engine-api.ts
+++ app/data/engine-api.ts
@@ -3,3 +3,14 @@ const BASE = '/neo/api/web-ide/';
 export const get = (url: string) => {
   return fetch(`${BASE}${url}`);
 };
+
+export const post = (url: string, data: unknown) => {
+  return fetch(`${BASE}${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-By': 'Neo'
+    },
+    body: JSON.stringify(data)
+  });
+};
