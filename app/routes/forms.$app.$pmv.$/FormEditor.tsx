409cb773bf133b6b8d1958ecf302e4fdc5e87e2a
Louis <louis.mueller@axonivy.com>
2024-06-17 09:47:49 +0200

    lli feedback

--- app/routes/forms.$app.$pmv.$/FormEditor.tsx
+++ app/routes/forms.$app.$pmv.$/FormEditor.tsx
@@ -3,13 +3,12 @@ import { Editor } from '~/neo/useEditors';
 import { ReadonlyProvider } from '@axonivy/ui-components';
 import { FormClient } from '@axonivy/form-editor-protocol';
 import { FormClientJsonRpc } from '@axonivy/form-editor-core';
-import { App, ClientContextProvider, QueryProvider, initQueryClient } from '@axonivy/form-editor';
+import { App, ClientContextProvider } from '@axonivy/form-editor';
 import { useLocation } from '@remix-run/react';
 
 const wsBase = () => window.location.origin.replace('https://', 'wss://').replace('http://', 'ws://');
 
 export const FormEditor = ({ id, app, pmv, path }: Editor) => {
-  const queryClient = initQueryClient();
   const [client, setClient] = useState<FormClient>();
   useEffect(() => {
     FormClientJsonRpc.startWebSocketClient(wsBase()).then(formClient => setClient(formClient));
@@ -28,11 +27,9 @@ export const FormEditor = ({ id, app, pmv, path }: Editor) => {
     <>
       {mounted && client && (
         <ClientContextProvider client={client}>
-          <QueryProvider client={queryClient}>
-            <ReadonlyProvider readonly={false}>
-              <App app={app} pmv={pmv} file={path.split('src_hd')[1]} />
-            </ReadonlyProvider>
-          </QueryProvider>
+          <ReadonlyProvider readonly={false}>
+            <App app={app} pmv={pmv} file={path.split('src_hd')[1]} />
+          </ReadonlyProvider>
         </ClientContextProvider>
       )}
     </>
