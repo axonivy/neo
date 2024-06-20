034cd19d12a88bf62e4ebef9cf955331661d8003
Lukas Lieb <lukas.lieb@axonivy.com>
2024-06-20 11:14:11 +0200

    Improve a11y of neo by
    - Focusable process/form cards and add enter key for opening
    - Change editor tabs to tab component (improve a11y + allow to navigate via arrowkeys etc.)

--- app/neo/card/ProjectArtifactCard.tsx
+++ app/neo/card/ProjectArtifactCard.tsx
@@ -21,13 +21,17 @@ const cardPreview = (editorType: EditorType) => {
 export const ProjectArtifactCard = ({ name, project, path, editorType, actions }: Card) => {
   const { openEditor, removeEditor } = useEditors();
   const id = editorId(editorType, project, path);
+  const open = () => openEditor({ id, type: editorType, icon: editorIcon(editorType), name, project, path });
   return (
     <Flex
+      role='group'
+      tabIndex={0}
       direction='column'
       justifyContent='space-between'
       gap={2}
-      style={{ background: 'var(--N75)', padding: 'var(--size-2)', borderRadius: 10, height: 150, flex: '0 1 200px' }}
-      onClick={() => openEditor({ id, type: editorType, icon: editorIcon(editorType), name, project, path })}
+      style={{ background: 'var(--N75)', padding: 'var(--size-2)', borderRadius: 10, height: 150, flex: '0 1 200px', cursor: 'pointer' }}
+      onClick={open}
+      onKeyDown={e => e.key === 'Enter' && open()}
     >
       <div style={{ background: 'var(--background)', borderRadius: 8, flex: '1 0 auto' }}>{cardPreview(editorType)}</div>
       <Flex alignItems='center' justifyContent='space-between' gap={1}>
@@ -35,7 +39,7 @@ export const ProjectArtifactCard = ({ name, project, path, editorType, actions }
         {actions ? (
           <Popover>
             <PopoverTrigger asChild>
-              <Button icon={IvyIcons.Dots} onClick={e => e.stopPropagation()} />
+              <Button icon={IvyIcons.Dots} onClick={e => e.stopPropagation()} onKeyDown={e => e.stopPropagation()} />
             </PopoverTrigger>
             <PopoverContent side='bottom' align='start' style={{ border: 'var(--basic-border)' }}>
               <Flex direction='column' alignItems='center' gap={2}>
