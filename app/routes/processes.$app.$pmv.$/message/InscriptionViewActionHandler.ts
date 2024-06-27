4682d3a0bc32fbdee9cb1126d0d7ca40ece79999
Louis <louis.mueller@axonivy.com>
2024-06-27 15:49:37 +0200

    XIVY-14102 update inscription view after creating process/form (#53)

--- app/routes/processes.$app.$pmv.$/message/InscriptionViewActionHandler.ts
+++ app/routes/processes.$app.$pmv.$/message/InscriptionViewActionHandler.ts
@@ -1,5 +1,5 @@
 import { useCallback } from 'react';
-import { InscriptionActionArgs } from '@axonivy/inscription-protocol';
+import { InscriptionActionArgs, InscriptionNotificationTypes } from '@axonivy/inscription-protocol';
 import { useNewArtifact } from '~/neo/artifact/useNewArtifact';
 import { useCreateProcess } from '~/data/process-api';
 import { ProjectIdentifier } from '~/data/project-api';
@@ -26,20 +26,16 @@ export const useNewProcessActionHandler = () => {
   const { createProcess } = useCreateProcess();
   const open = useNewArtifact();
   return useCallback(
-    (data: unknown) => {
+    (data: unknown, window: WindowProxy | null) => {
       if (!isActionWithId(data, 'newProcess')) {
         return;
       }
       const project = { app: data.params.context.app, pmv: data.params.context.pmv };
       const pid = data.params.context.pid;
-      open({
-        create: (name: string, namespace: string, project?: ProjectIdentifier, pid?: string) =>
-          createProcess({ name, namespace, kind: '', project, pid }),
-        defaultName: 'NewCallable',
-        title: 'Create new Process',
-        project,
-        pid
-      });
+      const postCreateAction = refreshInscriptionView(window);
+      const create = (name: string, namespace: string, project?: ProjectIdentifier, pid?: string) =>
+        createProcess({ name, namespace, kind: '', project, pid }, postCreateAction);
+      open({ create, defaultName: 'NewCallable', title: 'Create new Process', project, pid });
     },
     [createProcess, open]
   );
@@ -49,21 +45,28 @@ export const useNewFormActionHandler = () => {
   const { createForm } = useCreateForm();
   const open = useNewArtifact();
   return useCallback(
-    (data: unknown) => {
+    (data: unknown, window: WindowProxy | null) => {
       if (!isActionWithId(data, 'newHtmlDialog')) {
         return;
       }
       const project = { app: data.params.context.app, pmv: data.params.context.pmv };
       const pid = data.params.context.pid;
-      open({
-        create: (name: string, namespace: string, project?: ProjectIdentifier, pid?: string) =>
-          createForm({ name, namespace, type: 'Form', project, pid }),
-        defaultName: 'NewForm',
-        title: 'Create new Form',
-        project,
-        pid
-      });
+      const postCreateAction = refreshInscriptionView(window);
+      const create = (name: string, namespace: string, project?: ProjectIdentifier, pid?: string) =>
+        createForm({ name, namespace, type: 'Form', project, pid }, postCreateAction);
+      open({ create, defaultName: 'NewForm', title: 'Create new Form', project, pid });
     },
     [createForm, open]
   );
+};
+
+const refreshInscriptionView = (window: WindowProxy | null) => {
+  return () => {
+    sendInscriptionNotification(window, 'dataChanged');
+    sendInscriptionNotification(window, 'validation');
+  };
+};
+
+const sendInscriptionNotification = (window: WindowProxy | null, type: keyof InscriptionNotificationTypes) => {
+  window?.postMessage({ method: type });
 };
