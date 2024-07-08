c9726e46c47d6691b3a311fd7f26255aa6a2f799
Lukas Lieb <lukas.lieb@axonivy.com>
2024-07-08 13:58:08 +0200

    Add raise scripts

---
+++ .ivy/raise-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+mvn --batch-mode -f playwright/neo-test-project/pom.xml versions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f playwright/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
+
+npm install
+npm version -f --no-git-tag-version -ws --include-workspace-root ${1/SNAPSHOT/next}
+npm install
