1e72b2d8b34681b3fc23cc38f7456f9ed3e5c22e
Louis <louis.mueller@axonivy.com>
2024-07-31 15:26:49 +0200

    export button to link

--- app/neo/artifact/ImportDialog.tsx
+++ app/neo/artifact/ImportDialog.tsx
@@ -13,6 +13,7 @@ import {
   Input
 } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
+import { Link } from '@remix-run/react';
 import { ReactNode, useState } from 'react';
 
 export const ImportDialog = ({
@@ -27,6 +28,11 @@ export const ImportDialog = ({
   exportAction: () => void;
 }) => {
   const [file, setFile] = useState<File>();
+  const exportLink = (
+    <Link onClick={exportAction} to={'/'}>
+      Consider exporting the workspace beforehand.
+    </Link>
+  );
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -34,7 +40,7 @@ export const ImportDialog = ({
         <DialogHeader>
           <DialogTitle>Import Axon Ivy Projects into: {name}</DialogTitle>
           <DialogDescription>
-            The import cannot be undone. It could overwrite existing projects in {name}. Consider exporting the workspace beforehand.
+            The import cannot be undone. It could overwrite existing projects in {name}. {exportLink}
           </DialogDescription>
         </DialogHeader>
         <Flex direction='column' gap={2}>
@@ -58,9 +64,6 @@ export const ImportDialog = ({
               Import
             </Button>
           </DialogClose>
-          <Button variant='primary' size='large' onClick={() => exportAction()} icon={IvyIcons.Download}>
-            Export
-          </Button>
           <DialogClose asChild>
             <Button variant='outline' size='large' icon={IvyIcons.Close}>
               Cancel
