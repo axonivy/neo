c26dffd2ecb8c4e5039227691d3e8256ef6c7160
Lukas Lieb <lukas.lieb@axonivy.com>
2024-09-16 12:43:25 +0200

    Fix raise deps

--- .ivy/raise-deps.sh
+++ .ivy/raise-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" **/package.json
+sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" webviews/*/package.json
+sed -i -E "s/(\"@axonivy[^\"]*\"): \"[^\"]*\"/\1: \"~${1/SNAPSHOT/next}\"/" package.json
 npm run update:axonivy:next
 if [ "$DRY_RUN" = false ]; then
   npm install
