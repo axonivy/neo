bf9529e1c7a17681bbcf5cdba77d9b26c81ab546
Louis <louis.mueller@axonivy.com>
2024-10-02 16:54:56 +0200

    wip

--- app/neo/artifact/ArtifactGroup.tsx
+++ app/neo/artifact/ArtifactGroup.tsx
@@ -1,6 +1,6 @@
+import { Collapsible, CollapsibleContent, CollapsibleTrigger, Flex } from '@axonivy/ui-components';
 import { useParams } from '@remix-run/react';
 import { ReactNode } from 'react';
-import { ArtifactCollapsible } from './ArtifactCollapsible';
 
 type Group = {
   project: string;
@@ -23,3 +23,14 @@ export const ArtifactGroup = ({ project, newArtifactCard, children }: Group) => 
     </>
   );
 };
+
+const ArtifactCollapsible = ({ title, children }: { title: string; children: ReactNode }) => (
+  <Collapsible defaultOpen={true} style={{ width: '100%' }}>
+    <CollapsibleTrigger>{title}</CollapsibleTrigger>
+    <CollapsibleContent>
+      <Flex gap={4} style={{ flexWrap: 'wrap' }}>
+        {children}
+      </Flex>
+    </CollapsibleContent>
+  </Collapsible>
+);
--- app/routes/$ws.configurations._index/route.tsx
+++ app/routes/$ws.configurations._index/route.tsx
@@ -1,20 +1,12 @@
 import type { MetaFunction } from '@remix-run/node';
 import { useState } from 'react';
-import { ProjectIdentifier, useProjects } from '~/data/project-api';
+import { useProjects } from '~/data/project-api';
 import { ArtifactCard, cardLinks } from '~/neo/artifact/ArtifactCard';
+import { ArtifactGroup } from '~/neo/artifact/ArtifactGroup';
 import { useCreateEditor } from '~/neo/editors/useCreateEditor';
 import { Editor, useEditors } from '~/neo/editors/useEditors';
 import { Overview } from '~/neo/Overview';
 import PreviewSVG from './variables-preview.svg?react';
-
-type Variables = {
-  path: string;
-  project: ProjectIdentifier;
-};
-
-const toVariables: (project: ProjectIdentifier) => Variables = (project: ProjectIdentifier) => {
-  return { path: `${project.app}/${project.pmv}`, project };
-};
 
 export const links = cardLinks;
 
@@ -26,19 +18,23 @@ export default function Index() {
   const [search, setSearch] = useState('');
   const { data, isPending } = useProjects();
   const { createVariableEditor } = useCreateEditor();
-  const variables =
-    data?.map(project => toVariables(project)).filter(vars => vars.path.toLowerCase().includes(search.toLocaleLowerCase())) ?? [];
+  const projects = data?.filter(project => project.pmv.toLowerCase().includes(search.toLocaleLowerCase())) ?? [];
   return (
     <Overview title='Configurations' search={search} onSearchChange={setSearch} isPending={isPending}>
-      {variables.map(vars => {
-        const editor = createVariableEditor(vars.project);
-        return <VariablesCard key={editor.id} cardName={vars.path} {...editor} />;
+      {projects.map(project => {
+        const editor = createVariableEditor(project);
+        const card = <VariablesCard key={editor.id} {...editor} />;
+        return (
+          <ArtifactGroup project={project.pmv} newArtifactCard={<></>} key={project.pmv}>
+            {card}
+          </ArtifactGroup>
+        );
       })}
     </Overview>
   );
 }
 
-export const VariablesCard = ({ cardName, ...editor }: Editor & { cardName: string }) => {
+export const VariablesCard = ({ ...editor }: Editor) => {
   const { openEditor } = useEditors();
-  return <ArtifactCard name={cardName} type='variable' preview={<PreviewSVG />} onClick={() => openEditor(editor)} />;
+  return <ArtifactCard name={'variables'} type='variables' preview={<PreviewSVG />} onClick={() => openEditor(editor)} />;
 };
