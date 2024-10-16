5a944aaee62ac8c0ea790df747160da3c8a28742
Louis <louis.mueller@axonivy.com>
2024-10-16 09:43:37 +0200

    improve search: hide artifact groups if empty (#163)

--- app/neo/artifact/useFilteredGroups.tsx
+++ app/neo/artifact/useFilteredGroups.tsx
@@ -11,8 +11,8 @@ export function useFilteredGroups<T>(groups: ProjectGroup<T>[], artifactSearchTa
   const [search, setSearch] = useState('');
   const extendedGroups = useMemo(() => insertWorkspaceIfAbsent(groups ?? [], ws), [groups, ws]);
   const filteredGroups = useMemo(
-    () => filterArifacts(extendedGroups, t => artifactSearchTarget(t).toLocaleLowerCase().includes(search.toLocaleLowerCase())),
-    [artifactSearchTarget, extendedGroups, search]
+    () => filterArifacts(extendedGroups, t => artifactSearchTarget(t).toLocaleLowerCase().includes(search.toLocaleLowerCase()), ws),
+    [artifactSearchTarget, extendedGroups, search, ws]
   );
   return { filteredGroups, search, setSearch };
 }
@@ -24,9 +24,11 @@ function insertWorkspaceIfAbsent<T>(groups: ProjectGroup<T>[], workspace?: strin
   return groups;
 }
 
-function filterArifacts<T>(groups: ProjectGroup<T>[], filter: (artifact: T) => boolean) {
-  return groups.map(({ project, artifacts }) => {
-    const filteredArtifacts = artifacts.filter(proc => filter(proc));
-    return { project, artifacts: filteredArtifacts };
-  });
+function filterArifacts<T>(groups: ProjectGroup<T>[], filter: (artifact: T) => boolean, workspace?: string) {
+  return groups
+    .map(({ project, artifacts }) => {
+      const filteredArtifacts = artifacts.filter(proc => filter(proc));
+      return { project, artifacts: filteredArtifacts };
+    })
+    .filter(({ project, artifacts }) => artifacts.length > 0 || project === workspace);
 }
