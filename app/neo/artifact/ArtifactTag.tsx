f5702f85d3a8b7d5af2ef0713861b8eb87f29389
Louis <louis.mueller@axonivy.com>
2024-10-23 15:36:45 +0200

    remove fontweight

--- app/neo/artifact/ArtifactTag.tsx
+++ app/neo/artifact/ArtifactTag.tsx
@@ -1,5 +1,5 @@
 export const ArtifactTag = ({ label }: { label: string }) => (
   <div className='artifact-tag' style={{ background: 'var(--P50)', padding: 'var(--size-1)', borderRadius: 'var(--border-r2)' }}>
-    <span style={{ fontWeight: 500, color: 'var(--P300)' }}>{label}</span>
+    <span style={{ color: 'var(--P300)' }}>{label}</span>
   </div>
 );
