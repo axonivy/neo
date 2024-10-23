79bd0ae444b0d556fc512317be00b514d5d1769b
Louis <louis.mueller@axonivy.com>
2024-10-23 15:33:47 +0200

    feedback

--- app/neo/artifact/ArtifactTag.tsx
+++ app/neo/artifact/ArtifactTag.tsx
@@ -1,5 +1,5 @@
 export const ArtifactTag = ({ label }: { label: string }) => (
-  <div className='artifact-tag' style={{ background: 'var(--P50)', color: 'red', padding: 5, borderRadius: 5 }}>
+  <div className='artifact-tag' style={{ background: 'var(--P50)', padding: 'var(--size-1)', borderRadius: 'var(--border-r2)' }}>
     <span style={{ fontWeight: 500, color: 'var(--P300)' }}>{label}</span>
   </div>
 );
