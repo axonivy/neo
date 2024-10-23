32f915b30ff58a1f18bc2769d84eeb96b23a06d5
Louis <louis.mueller@axonivy.com>
2024-10-23 16:19:50 +0200

    feedback

--- app/neo/useSearch.tsx
+++ app/neo/useSearch.tsx
@@ -1,13 +1,10 @@
 import { useSearchParams } from '@remix-run/react';
-import { useState } from 'react';
 
 export const useSearch = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [search, setSearch] = useState(searchParams.get('search') ?? '');
   return {
-    search,
+    search: searchParams.get('search') ?? '',
     setSearch: (change: string) => {
-      setSearch(change);
       setSearchParams({ search: change }, { replace: true });
     }
   };
