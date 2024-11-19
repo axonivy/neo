a8796c0ecbf9633759db3b7252c9f1630c6e09f5
Louis <louis.mueller@axonivy.com>
2024-11-19 15:52:44 +0100

    fix test

--- playwright/tests/screenshots/overviews.spec.ts
+++ playwright/tests/screenshots/overviews.spec.ts
@@ -9,7 +9,7 @@ import { screenshot } from './screenshot-util';
 test('workspaces', async ({ page }) => {
   await Neo.open(page);
   const overview = new Overview(page);
-  await expect(overview.title).toHaveText(/Welcome to Axon Ivy NEO Designer/);
+  await expect(page.getByText('Welcome to Axon Ivy NEO Designer: Manage your workspaces')).toBeVisible();
   await overview.expectCardsCountGreaterThan(0);
   await screenshot(page, 'overview-workspaces');
 });
