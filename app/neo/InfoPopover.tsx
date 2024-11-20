4f1a5f4a54cf5f59c4137b2adaacecfa48c8ae2b
Louis <louis.mueller@axonivy.com>
2024-11-20 08:48:27 +0100

    w

--- app/neo/InfoPopover.tsx
+++ app/neo/InfoPopover.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react';
 export const InfoPopover = ({ info, children }: { info: string; children?: ReactNode }) => (
   <Popover>
     <PopoverTrigger asChild>
-      {children ? children : <Button size='small' style={{ color: 'var(--P300)' }} icon={IvyIcons.InfoCircle} />}
+      {children ?? <Button size='small' style={{ color: 'var(--P300)' }} icon={IvyIcons.InfoCircle} />}
     </PopoverTrigger>
     <PopoverContent sideOffset={10} align='start' collisionPadding={20} style={{ borderRadius: '10px', maxWidth: 383, padding: 15 }}>
       <span style={{ lineHeight: '22px', fontSize: 16, fontWeight: 400, color: 'var(--N900)' }}>{info}</span>
