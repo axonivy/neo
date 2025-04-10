d5389e4d57d6abcdb2c58db03d27ee772e97b25e
Lukas Giger <lukas.giger@axonivy.com>
2025-04-10 06:40:47 +0200

    XIVY-15943 fix configs tests
    Adjust tests after removing the CMS files from being returned by the core endpoint `/configurations`.

--- playwright/tests/integration/configs.spec.ts
+++ playwright/tests/integration/configs.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-import { MonacoEditor } from '../page-objects/monaco-editor';
+import { CmsEditor } from '../page-objects/cms-editor';
 import { Neo } from '../page-objects/neo';
 import { VariableEditor } from '../page-objects/variables-editor';
 import { TEST_PROJECT } from './constants';
@@ -11,17 +11,17 @@ test('navigate to configs', async ({ page }) => {
   await overview.card('variables').click();
   await new VariableEditor(neo, 'variables').expectOpen('MyVar');
   await neo.configs();
-  await overview.card('cms_de-ch').click();
-  await new MonacoEditor(neo, 'cms_de-ch').expectOpen('ReleaseDate: Release Date');
+  await overview.card('cms').click();
+  await new CmsEditor(neo, 'cms').expectOpen('/Labels/ReleaseDate');
 });
 
 test('search configs', async ({ page }) => {
   const neo = await Neo.openWorkspace(page);
   const overview = await neo.configs();
-  await expect(overview.cards).toHaveCount(3);
+  await expect(overview.cards).toHaveCount(2);
   await overview.search.fill('bla');
   await expect(overview.cards).toHaveCount(0);
 
   await overview.search.fill('cms');
-  await expect(overview.cards).toHaveCount(2);
+  await expect(overview.cards).toHaveCount(1);
 });
