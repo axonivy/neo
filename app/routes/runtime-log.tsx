81ae52dcef3f4f277490b342f04a7277c605434d
ivy-tgr <tobias.gretler@axonivy.com>
2025-04-16 08:03:58 +0200

    XIVY-16478  Add translation  of Runtimelog

--- app/routes/runtime-log.tsx
+++ app/routes/runtime-log.tsx
@@ -1,6 +1,8 @@
 import { App, ClientContextProvider } from '@axonivy/log-view';
 import viewStylesHref from '@axonivy/log-view/lib/view.css?url';
 import { Spinner, ThemeProvider } from '@axonivy/ui-components';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
 import type { LinksFunction, MetaFunction } from 'react-router';
 import { NEO_DESIGNER } from '~/constants';
 import { RuntimeLogClientNeo } from '../neo/editors/runtime-log-client';
@@ -23,7 +25,9 @@ export default function Index() {
     <div className='runtime-log' style={{ height: '100%' }}>
       <ClientContextProvider client={client}>
         <ThemeProvider disabled>
-          <App />
+          <I18nextProvider i18n={i18next} defaultNS={'log-view'}>
+            <App />
+          </I18nextProvider>
         </ThemeProvider>
       </ClientContextProvider>
     </div>
