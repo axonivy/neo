abe63bb0166617714bc46d5bc543876124fdb716
ivy-tgr <tobias.gretler@axonivy.com>
2025-04-16 08:26:40 +0200

    XIVY-16478 translation of Runtime log

--- app/translation/translation.ts
+++ app/translation/translation.ts
@@ -8,6 +8,7 @@ import { initReactI18next } from 'react-i18next';
 import { enTranslation as translationCms } from '@axonivy/cms-editor';
 import { enTranslation as translationDataClass } from '@axonivy/dataclass-editor';
 import { enMessages as translationForm } from '@axonivy/form-editor';
+import { enMessages as translationlog } from '@axonivy/log-view';
 import { enTranslation as translationVariable } from '@axonivy/variable-editor';
 import enTranslationNeo from './neo/en.json';
 
@@ -16,7 +17,8 @@ const localTranslations: Resource = {
   'dataclass-editor': { en: translationDataClass },
   'cms-editor': { en: translationCms },
   'variable-editor': { en: translationVariable },
-  'form-editor': { en: translationForm }
+  'form-editor': { en: translationForm },
+  'log-view': { en: translationlog }
 };
 
 const LOCALES_PATH = `${import.meta.env.BASE_URL ?? '/'}assets/locales`;
