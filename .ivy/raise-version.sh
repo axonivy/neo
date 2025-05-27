988441276e64271baecab830f74ac3a994fdf976
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-05-27 11:45:18 +0200

    fix; raise-version fails if run repeatedly with the same version

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -7,5 +7,5 @@ mvn --batch-mode -f playwright/neo-test-project/pom.xml versions:set versions:co
 mvn --batch-mode -f playwright/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
 
 npm install
-npm version -f --no-git-tag-version -ws --include-workspace-root ${1/SNAPSHOT/next}
+npm version -f --allow-same-version --no-git-tag-version -ws --include-workspace-root ${1/SNAPSHOT/next}
 npm install
