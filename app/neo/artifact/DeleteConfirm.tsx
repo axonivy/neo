81e47987253182bb9af482b5b47e7d5ace5cfefb
ivy-jh <jens.hartmann@axonivy.com>
2025-07-03 22:12:11 +0200

    XIVY-16606 streamline dialog

--- app/neo/artifact/DeleteConfirm.tsx
+++ app/neo/artifact/DeleteConfirm.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@axonivy/ui-components';
+import { BasicDialog, Button } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { useTranslation } from 'react-i18next';
 
@@ -17,26 +17,23 @@ export const DeleteConfirm = ({
 }) => {
   const { t } = useTranslation();
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{deleteAction.isDeletable ? t('artifact.deleteConfirm', { artifact: title }) : deleteAction.message}</DialogTitle>
-        </DialogHeader>
-        <DialogFooter>
-          {deleteAction.isDeletable && (
-            <DialogClose asChild>
-              <Button variant='primary' size='large' onClick={deleteAction.run} icon={IvyIcons.Trash}>
-                {t('common.label.delete')}
-              </Button>
-            </DialogClose>
-          )}
-          <DialogClose asChild>
-            <Button variant='outline' size='large' icon={IvyIcons.Close}>
-              {t('common.label.cancel')}
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <BasicDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      contentProps={{
+        title: t('artifact.deleteConfirmTitle'),
+        description: deleteAction.isDeletable ? t('artifact.deleteConfirmMsg', { artifact: title }) : deleteAction.message,
+        buttonClose: (
+          <Button variant='outline' size='large'>
+            {t('common.label.cancel')}
+          </Button>
+        ),
+        buttonCustom: deleteAction.isDeletable && (
+          <Button variant='primary' size='large' onClick={deleteAction.run} icon={IvyIcons.Trash}>
+            {t('common.label.delete')}
+          </Button>
+        )
+      }}
+    ></BasicDialog>
   );
 };
