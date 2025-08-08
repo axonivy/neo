41a60ce196851589410eb418d1e1981c0686f34c
Lukas Lieb <lukas.lieb@axonivy.com>
2025-08-08 12:05:02 +0200

    XIVY-17303 Breadcrumb project switch menu

--- app/neo/Breadcrumb.tsx
+++ app/neo/Breadcrumb.tsx
@@ -1,39 +1,80 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@axonivy/ui-components';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  Flex
+} from '@axonivy/ui-components';
+import { IvyIcons } from '@axonivy/ui-icons';
+import type { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink, useParams } from 'react-router';
 import { Fragment } from 'react/jsx-runtime';
 
+type BreadcrumbItem = { name: string; href?: string; menu?: ReactNode };
+
 type BreadcrumbProps = {
   style?: React.CSSProperties;
-  items?: Array<{ name: string; href?: string }>;
+  items?: Array<BreadcrumbItem>;
 };
 
 export const Breadcrumbs = ({ style, items = [] }: BreadcrumbProps) => {
   const { t } = useTranslation();
   const { ws } = useParams();
-  const workspacesItem = { name: t('neo.workspaces'), href: '' };
-  const wsItem = { name: ws, href: ws };
+  const workspacesItem: BreadcrumbItem = { name: t('neo.workspaces'), href: '' };
+  const wsItem: BreadcrumbItem = { name: ws ?? '', href: ws };
   const breadcrumbItems = [workspacesItem, wsItem, ...items];
   const lastItem = breadcrumbItems.pop();
   return (
     <Breadcrumb style={style ?? { fontSize: 12 }}>
       <BreadcrumbList>
-        {breadcrumbItems?.map(({ name, href }) => (
-          <Fragment key={`${name}-${href}`}>
+        {breadcrumbItems?.map(item => (
+          <Fragment key={`${item.name}-${item.href}`}>
             <BreadcrumbItem>
-              {href !== undefined ? (
-                <BreadcrumbLink asChild>
-                  <NavLink to={`/${href ?? ''}`}>{name}</NavLink>
-                </BreadcrumbLink>
-              ) : (
-                name
-              )}
+              <BreadcrumbPart {...item} />
             </BreadcrumbItem>
             <BreadcrumbSeparator />
           </Fragment>
         ))}
-        <BreadcrumbPage>{lastItem?.name}</BreadcrumbPage>
+        {lastItem && (
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              <BreadcrumbPart {...lastItem} />
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        )}
       </BreadcrumbList>
     </Breadcrumb>
   );
+};
+
+const BreadcrumbPart = ({ name, href, menu }: BreadcrumbItem) => {
+  let item: ReactNode = name;
+  if (href !== undefined) {
+    item = (
+      <BreadcrumbLink asChild>
+        <NavLink to={`/${href ?? ''}`}>{name}</NavLink>
+      </BreadcrumbLink>
+    );
+  }
+  if (menu) {
+    return (
+      <Flex direction='row' alignItems='center' gap={1}>
+        {item}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button icon={IvyIcons.Chevron} rotate={90} style={{ height: 16 }} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>{menu}</DropdownMenuContent>
+        </DropdownMenu>
+      </Flex>
+    );
+  }
+  return item;
 };
--- playwright/tests/integration/breadcrumbs.spec.ts
+++ playwright/tests/integration/breadcrumbs.spec.ts
@@ -2,23 +2,49 @@ import test, { expect } from '@playwright/test';
 import { DataClassEditor } from '../page-objects/data-class-editor';
 import { Neo } from '../page-objects/neo';
 import { Overview } from '../page-objects/overview';
-import { APP, TEST_PROJECT } from './constants';
+import { APP, TEST_PROJECT, WORKSPACE } from './constants';
 
-test('navigate via breadcrumbs', async ({ page }) => {
+test('editor breadcrumbs', async ({ page }) => {
   const neo = await Neo.openWorkspace(page, `dataclasses/${APP}/${TEST_PROJECT}/dataclasses/neo/test/project/QuickStartTutorial`);
   const editor = new DataClassEditor(neo, 'QuickStartTutorial');
   await editor.expectOpen('product');
-  await neo.breadcrumbs.expectItems(['Workspaces', 'neo-test-project', 'Data Classes', 'neo-test-project', 'QuickStartTutorial']);
+  await neo.breadcrumbs.expectItems(['Workspaces', WORKSPACE, 'Data Classes', TEST_PROJECT, 'QuickStartTutorial']);
 
-  await neo.breadcrumbs.item('Data Classes').click();
+  await neo.breadcrumbs.item(TEST_PROJECT).last().click();
   const overview = new Overview(page);
   await expect(overview.title).toHaveText('Data Classes');
-  await neo.breadcrumbs.expectItems(['Workspaces', 'neo-test-project', 'Data Classes']);
+  await expect(overview.filter.filterTag(TEST_PROJECT)).toBeVisible();
+  await neo.breadcrumbs.expectItems(['Workspaces', WORKSPACE, 'Data Classes']);
+  await page.goBack();
 
-  await neo.breadcrumbs.item('neo-test-project').click();
+  await neo.breadcrumbs.item('Data Classes').click();
+  await expect(overview.title).toHaveText('Data Classes');
+  await expect(overview.filter.filterTag(TEST_PROJECT)).toBeHidden();
+  await neo.breadcrumbs.expectItems(['Workspaces', WORKSPACE, 'Data Classes']);
+
+  await neo.breadcrumbs.item(WORKSPACE).click();
   await expect(overview.title).toHaveText('Projects');
 
   await neo.breadcrumbs.item('Workspaces').click();
   await expect(overview.title).toHaveText('Manage your workspaces');
   await expect(neo.breadcrumbs.navigation).toBeHidden();
+});
+
+test('project breadcrumbs', async ({ page }) => {
+  const neo = await Neo.openWorkspace(page, `projects/${APP}/${TEST_PROJECT}`);
+  const overview = new Overview(page);
+  await expect(overview.infoTitle).toHaveText(`Project details: ${TEST_PROJECT}`);
+  await neo.breadcrumbs.expectItems(['Workspaces', WORKSPACE, 'Projects', TEST_PROJECT]);
+
+  await neo.breadcrumbs.item(TEST_PROJECT).last().getByRole('button').focus();
+  await page.keyboard.press('Enter');
+  await expect(page.getByRole('menu')).toBeVisible();
+  // Fixme after create project is supported
+
+  // const otherProject = page.getByRole('menu').getByRole('menuitem', { name: 'quick-start-tutorial' });
+  // await expect(otherProject).toBeVisible();
+  // await otherProject.click();
+
+  // await expect(overview.infoTitle).toHaveText(`Project details: quick-start-tutorial`);
+  // await neo.breadcrumbs.expectItems(['Workspaces', WORKSPACE, 'Projects', 'quick-start-tutorial']);
 });
