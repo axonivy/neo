7e5d094f912458e9d8939b17a9f9b58d6af46aca
ivy-jh <jens.hartmann@axonivy.com>
2025-08-19 16:00:55 +0200

    wip

--- app/routes/workspace/ProjectCreator.test.tsx
+++ app/routes/workspace/ProjectCreator.test.tsx
@@ -1,10 +1,10 @@
-import { NameChangeHandler } from './ProjectCreator';
+import { nameChangeHandler } from './ProjectCreator';
 
 test('projectdetails name handling', () => {
-  expect(NameChangeHandler('M', '', '.')).toEqual('m');
-  expect(NameChangeHandler('HALLO', 'HALL', '.')).toEqual('hallo');
-  expect(NameChangeHandler('Mein-', 'mein', '.')).toEqual('mein.');
-  expect(NameChangeHandler('P-r-o-j-E-K-T-', 'P-r-o-j-E-K-T--', '-')).toEqual('p-r-o-j-e-k-t-');
-  expect(NameChangeHandler('PROJEKTNAME', '', '.')).toEqual('projektname');
-  expect(NameChangeHandler('projektName', 'dontTouchIt', '.')).toEqual(undefined);
+  expect(nameChangeHandler('M', '', '.')).toEqual('m');
+  expect(nameChangeHandler('HALLO', 'HALL', '.')).toEqual('hallo');
+  expect(nameChangeHandler('Mein-', 'mein', '.')).toEqual('mein.');
+  expect(nameChangeHandler('P-r-o-j-E-K-T-', 'P-r-o-j-E-K-T--', '-')).toEqual('p-r-o-j-e-k-t-');
+  expect(nameChangeHandler('PROJEKTNAME', '', '.')).toEqual('projektname');
+  expect(nameChangeHandler('projektName', 'dontTouchIt', '.')).toEqual(undefined);
 });
