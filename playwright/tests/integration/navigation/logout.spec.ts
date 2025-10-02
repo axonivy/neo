cb9c2ea1259fcd6867f56dc7dcdd194229469d20
ivy-tgr <tobias.gretler@axonivy.com>
2025-10-02 16:19:26 +0200

    XIVY 15775 Show current username

--- playwright/tests/integration/navigation/logout.spec.ts
+++ playwright/tests/integration/navigation/logout.spec.ts
@@ -7,6 +7,7 @@ test('logout User', async ({ page }) => {
   await expect(page.getByRole('menuitem', { name: 'Developer' })).toBeVisible();
   await page.getByRole('menuitem', { name: 'Developer' }).click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
-  await expect(page.getByRole('button', { name: 'Developer' })).toBeVisible();
+  await page.getByRole('button', { name: 'Settings' }).click();
+  await expect(page.getByRole('menuitem', { name: 'Developer' })).toBeVisible();
   // Because of the auto login in the test setup, we are again logged in after logout.
 });
