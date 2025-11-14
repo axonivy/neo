4a8372d3f8b7d2c018de9576eb59aba38ec8adf9
Lukas Lieb <lukas.lieb@axonivy.com>
2025-11-14 16:12:45 +0100

    XIVY-17778 Redirect to login page

--- app/data/user-api.ts
+++ app/data/user-api.ts
@@ -2,7 +2,7 @@ import { toast } from '@axonivy/ui-components';
 import { useQuery } from '@tanstack/react-query';
 import i18next from 'i18next';
 import { ok } from './custom-fetch';
-import { me1 } from './generated/ivy-client';
+import { logoutMe, me1 } from './generated/ivy-client';
 
 export const useUser = () =>
   useQuery({
@@ -15,10 +15,21 @@ export const useUser = () =>
       toast.error(i18next.t('toast.unauthorized'), {
         duration: Infinity,
         toasterId: 'endless',
-        action: { label: i18next.t('common.label.reload'), onClick: () => window.location.reload() }
+        action: { label: i18next.t('common.label.reload'), onClick: () => redirectToLogin() }
       });
       return null;
     },
     refetchInterval: 3 * 60 * 1000,
     refetchOnWindowFocus: true
   });
+
+export const logout = () => {
+  logoutMe().then(() => {
+    redirectToLogin();
+  });
+};
+
+const redirectToLogin = () => {
+  window.location.href = '/go/login?originalUrl=/neo/';
+  window.location.reload();
+};
