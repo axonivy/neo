a7e50eaf8af1032bf67b04ea0b13fb31814ac019
Lukas Lieb <lukas.lieb@axonivy.com>
2026-03-12 08:02:32 +0100

    Fix unhandled aciton

--- app/neo/editors/restclient/useActionHandler.ts
+++ app/neo/editors/restclient/useActionHandler.ts
@@ -1,20 +1,26 @@
+import { toast } from '@axonivy/ui-components';
 import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useOpenUrl } from '~/neo/browser/useOpenUrl';
 import { noUnknownAction } from '~/utils/no-unknown';
 import type { RestClientActionHandler } from './restclient-client';
 
 export const useActionHandler = () => {
+  const { t } = useTranslation();
   const openUrl = useOpenUrl();
   return useCallback<RestClientActionHandler>(
     action => {
       switch (action.actionId) {
         case 'openUrl':
-          openUrl(action.payload);
+          openUrl(action.payload as string);
+          return;
+        case 'generateOpenApiClient':
+          toast.warning(t('message.unsupportedAction', { action: action.actionId }));
           return;
         default:
           noUnknownAction(action.actionId);
       }
     },
-    [openUrl]
+    [openUrl, t]
   );
 };
