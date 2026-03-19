960819e6e989f3e65d0df4896fc54a5610d3ed83
Louis <louis.mueller@axonivy.com>
2026-03-19 14:06:12 +0100

    update i18n

--- i18next.config.ts
+++ i18next.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'i18next-cli';
 
 export default defineConfig({
-  locales: ['en', 'de'],
+  locales: ['en', 'de', 'ja'],
   extract: {
     input: ['app/**/*.ts', 'app/**/*.tsx'],
     output: 'app/translation/{{namespace}}/{{language}}.json',
