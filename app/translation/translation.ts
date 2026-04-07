58f9aee1de9e1cf7e20bfe4d35ff408edace6254
Lukas Lieb <lukas.lieb@axonivy.com>
2026-04-07 09:27:26 +0200

    Remove showSupportNotice option from i18n initialization in process-editor

--- app/translation/translation.ts
+++ app/translation/translation.ts
@@ -51,7 +51,6 @@ export const initTranslation = async (debug = false) => {
     .use(LngDetector)
     .init({
       debug,
-      showSupportNotice: false,
       fallbackLng: 'en',
       ns: Object.keys(localTranslations),
       defaultNS: 'neo',
