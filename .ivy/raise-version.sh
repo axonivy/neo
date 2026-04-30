474903e6d837c72aa17bacd691319b4fcdf3d599
Lukas Lieb <lukas.lieb@axonivy.com>
2026-04-30 14:31:46 +0200

    Update to pnpm 11

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -7,5 +7,5 @@ mvn --batch-mode -f playwright/neo-test-project/pom.xml versions:set versions:co
 mvn --batch-mode -f playwright/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
 
 pnpm install
-pnpm version --allow-same-version --no-git-tag-version -ws --include-workspace-root --workspaces-update=false ${1/SNAPSHOT/next}
+pnpm version -r --allow-same-version --no-git-tag-version ${1/SNAPSHOT/next}
 pnpm install --no-frozen-lockfile
