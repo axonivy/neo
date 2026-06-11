80c955b013ed89238790d2707791306e1c780c51
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-06-11 14:53:56 +0200

    XIVY-18535 bury codegen action
    - it's now a function

--- app/neo/editors/restclient/useActionHandler.ts
+++ app/neo/editors/restclient/useActionHandler.ts
@@ -1,12 +1,9 @@
-import { toast } from '@axonivy/ui-components';
 import { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useOpenUrl } from '~/neo/browser/useOpenUrl';
 import { noUnknownAction } from '~/utils/no-unknown';
 import type { RestClientActionHandler } from './restclient-client';
 
 export const useActionHandler = () => {
-  const { t } = useTranslation();
   const openUrl = useOpenUrl();
   return useCallback<RestClientActionHandler>(
     action => {
@@ -14,13 +11,10 @@ export const useActionHandler = () => {
         case 'openUrl':
           openUrl(action.payload as string);
           return;
-        case 'generateOpenApiClient':
-          toast.warning(t('message.unsupportedAction', { action: action.actionId }));
-          return;
         default:
           noUnknownAction(action.actionId);
       }
     },
-    [openUrl, t]
+    [openUrl]
   );
 };
